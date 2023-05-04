83f94d3c0fc57539c89c6c6e4881fd853340fe4c
besher <beshoux@outlook.fr>
2023-05-04 04:29:35 +0200

    Get all the data from all the books.

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -9,11 +9,13 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css("article.product_pod")
 
         for book in books:
-            yield {
-                    'name': book.css("h3 a::text"),
-                    'price': book.css(".product_price .price_color::text"),
-                    'url': book.css("h3 a").attrib['href'],
-                    }
+            next_book = book.css('h3 a ::attr(href)').get()
+            if next_book is not None:
+                if 'catalogue/' in next_book:
+                    next_book_url = 'http://books.toscrape.com/' + next_book
+                else:
+                    next_book_url = 'http://books.toscrape.com/catalogue/' + next_book
+                yield response.follow(next_book_url, callback = self.parse_book_page)
 
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
@@ -22,3 +24,20 @@ class BookspiderSpider(scrapy.Spider):
             else:
                 next_page_url = 'http://books.toscrape.com/catalogue/' + next_page
             yield response.follow(next_page_url, callback = self.parse)
+
+    def parse_book_page(self, response):
+        table_rows = response.css('table tr')
+        yield {
+                'url': response.url,
+                'title': response.css('.product_main h1::text').get(),
+                'product_type': table_rows[1].css("td ::text").get(),
+                'price_excl_tax': table_rows[2].css("td ::text").get(),
+                'price_incl_tax': table_rows[3].css("td ::text").get(),
+                'tax': table_rows[4].css("td ::text").get(),
+                'availabilty': table_rows[5].css("td ::text").get(),
+                'num_reviews': table_rows[6].css("td ::text").get(),
+                'stars': response.css("p.star-rating").attrib['class'],
+                'category': response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
+                'price': response.css("p.price_color::text").get()
+                }
+        pass
