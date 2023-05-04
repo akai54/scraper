1a1056f97ffba0044b3bef14ba202226ad8763d6
besher <beshoux@outlook.fr>
2023-05-04 21:40:46 +0200

    Fix spelling errors.

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -7,24 +7,25 @@ class BookscraperPipeline:
         # Strip all the whitespaces from the strings
         field_names = adapter.field_names()
         for field_name in field_names:
-            if field_name != 'descryption':
+            if field_name != 'description':
                 value = adapter.get(field_name)
                 adapter[field_name] = value[0].strip()
 
         # To lowercase
-        lowercase_fields_names = ['category', 'product_type']
-        for lowercase_field_name in lowercase_fields_names:
-            value = adapter.get(lowercase_field_name)
-            adapter[lowercase_field_name] = value.lower()
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            print(lowercase_key, value)
+            adapter[lowercase_key] = value.lower()
 
         # Keep only the number of the books
-        availabilty_string = adapter.get('availabilty')
-        split_string_array = availabilty_string.split('()')
+        availability_string = adapter.get('availability')
+        split_string_array = availability_string.split('(')
         if len(split_string_array) < 2:
-            adapter['availabilty'] = 0
+            adapter['availability'] = 0
         else:
-            availabilty_array = split_string_array[1].split(' ')
-            adapter['availabilty'] = int(availabilty_array[0])
+            availability_array = split_string_array[1].split(' ')
+            adapter['availability'] = int(availability_array[0])
 
         # Convert the number of reviews to an int
         num_reviews_string = adapter.get('num_reviews')
